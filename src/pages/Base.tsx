b6f0c80bbeca30d9e64fc885cc95c7c59ed1190a
Sam <stsmithe@ncsu.edu>
2026-02-20 19:20:19 -0500

    added in development temp page

---
+++ src/pages/Base.tsx
@@ -0,0 +1,18 @@
+import '../styles/Base.css'
+
+export default function Base() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1 className="heading">🚧 In Development</h1>
+        <p className="message">
+          This page is not finished. Check back soon!
+        </p>
+
+        <a href="https://samsmithers.com">
+          <button>Go Back</button>
+        </a>
+      </div>
+    </div>
+  );
+}
