24fd895f51ac6811a2d65c3cec486cb43c7579fc
Sam <stsmithe@ncsu.edu>
2026-03-02 15:44:24 -0500

    div spacing and creation of home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,33 @@ import Background from '../components/Background'
 import Navbar from '../components/Navbar'
 
 export default function Home() {
+  document.body.style.overflowY = "hidden";
   return (
     <>
+      <Navbar highlight="home" disabled="home" />
       <div className="background-container">
         <Background />
       </div>
-      <Navbar highlight="home" disabled="home" />
-      <div className="container">
+      <div className="home-content">
+        
+        <div className="home-card home-recent">
+          {/* Insert recent content additions info fetch */}
+          <h1>Recent</h1>
+        </div>
 
+        <div className="home-right">
+          <div className="home-card home-profile-card">
+            {/* Insert profile shtuff */}
+            <h1>Profile Card</h1>
+          </div>
+          <div className="home-card home-uptime-card">
+            {/* Insert uptime fetch of current projects. */}
+            {/* Prioritize recently updated projects when choosing 
+            which to show uptime for, if not enough space */}
+            <h1>Uptime Card</h1>
+          </div>
+        </div>
+        
       </div>
     </>
   )
