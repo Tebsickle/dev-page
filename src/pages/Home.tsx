f9502a94fc052727c7a7cf52b852f23eaa333886
Sam <tebs2000@gmail.com>
2026-03-16 00:54:43 -0400

    add profile card to home. add favicon. change site font (subject to change later)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,8 +18,17 @@ export default function Home() {
         <div className="home-right">
           <div className="home-card home-profile-card">
             {/* Insert profile shtuff */}
-            <h1 className="recent-header">Tebs</h1>
-            <p style={{ textAlign: "center" }}>Not implemented yet!</p>
+            <div className="profile-header">
+              <img className="profile-pic" src="/src/assets/pfp.png" alt="Profile Picture" />
+              <div className="profile-nametags">
+                <h1>Sam Smithers</h1>
+                <h3>@Tebs</h3>
+              </div>
+            </div>
+            <p className="profile-bio">
+              I'm a computer science student and developer who loves creating.
+              I build small apps, games, and devices, 
+              always exploring new ideas and learning through projects.</p>
           </div>
           <div className="home-card home-uptime-card">
             {/* Insert uptime fetch of current projects. */}
