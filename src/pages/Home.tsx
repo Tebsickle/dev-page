df9a11553999caf2f78304a87d3dd1f0148a9b7b
Sam <tebs2000@gmail.com>
2026-03-16 23:59:52 -0400

    profile card done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import '../styles/Home.css'
 import Navbar from '../components/Navbar'
 import RecentFetch from '../components/home/RecentFetch'
+import fallbackPfp from '../assets/home/pfp.png'
+import GitHubIcon from '../assets/home/github.svg'
+import LinkedInIcon from '../assets/home/linkedin.png'
 
 export default function Home() {
   document.body.style.overflowY = "hidden";
@@ -15,21 +18,29 @@ export default function Home() {
 
         </div>
 
+        {/* Right Cards */}
         <div className="home-right">
+          {/* Profile card */}
           <div className="home-card home-profile-card">
-            {/* Insert profile shtuff */}
             <div className="profile-header">
-              <img className="profile-pic" src="/src/assets/pfp.png" alt="Profile Picture" />
+              <img className="profile-pic" src={`https://github.com/Tebsickle.png`} alt="Profile Picture" onError={(e) => { (e.currentTarget as HTMLImageElement).src = fallbackPfp }} />
               <div className="profile-nametags">
                 <h1>Sam Smithers</h1>
-                <h3>@Tebs</h3>
+                <h3>@Tebs/@Tebsickle</h3>
               </div>
             </div>
             <p className="profile-bio">
               I'm a computer science student and developer who loves creating.
               I build small apps, games, and devices, 
-              always exploring new ideas and learning through projects.</p>
+              always exploring new ideas and learning through projects.
+            </p>
+            <div className="contact-icons-div">
+              <img className="contact-icons" src={GitHubIcon} alt="GitHub" onClick={() => window.open('https://github.com/Tebsickle', '_blank')} />
+              <img className="contact-icons" src={LinkedInIcon} alt="LinkedIn" onClick={() => window.open('https://www.linkedin.com/in/samuel-smithers-833451346', '_blank')} />
+            </div>
           </div>
+          
+          {/* Uptime card */}
           <div className="home-card home-uptime-card">
             {/* Insert uptime fetch of current projects. */}
             {/* Prioritize recently updated projects when choosing 
